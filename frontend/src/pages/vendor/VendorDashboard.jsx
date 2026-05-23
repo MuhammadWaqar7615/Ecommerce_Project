a327c9859c1835cfaf08fb307da9a3ab15f770d2
MuhammadWaqar7615 <muhammadwaqar7615@gmail.com>
2026-05-23 11:59:05 -0700

    fix: update greeting text in VendorDashboard component

--- frontend/src/pages/vendor/VendorDashboard.jsx
+++ frontend/src/pages/vendor/VendorDashboard.jsx
@@ -9,7 +9,7 @@ const VendorDashboard = ({ children }) => {
     >
       <div className="bg-white rounded-lg shadow-lg p-6">
         {children}
-        hello
+        hello world
       </div>
     </DashboardLayout>
   );

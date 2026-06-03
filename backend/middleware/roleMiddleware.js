d0720d773f15226e04b9928faac14b85294f0b1c
MuhammadWaqar7615 <mwaqar7615@gmail.com>
2026-06-03 19:04:30 +0500

    add debugging log for user role in roleMiddleware

--- backend/middleware/roleMiddleware.js
+++ backend/middleware/roleMiddleware.js
@@ -2,6 +2,7 @@ const { errorResponse } = require('../utils/apiResponse');
 
 const authorize = (...roles) => {
   return (req, res, next) => {
+    console.log('User role:', req.user.role); // Debugging line
     if (!roles.includes(req.user.role)) {
       return errorResponse(res, `Access denied. ${req.user.role} not authorized`, 403);
     }
